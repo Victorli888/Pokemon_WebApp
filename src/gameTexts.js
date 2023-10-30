ab592be5a1bf5d54f62c02ab3ee14675fd3e6024
victorli <victorli7462@gmail.com>
2023-10-30 11:50:26 +0800

    Create single resource for game text to use

---
+++ src/gameTexts.js
@@ -0,0 +1,7 @@
+export var  gameTexts = {
+    wildPokemonIntro: "A wild Pokemon has Appeared!",
+    escapeSuccess: "You have successfully run away!",
+    escapeFail: "You are not able to run away from a trainer battle.",
+    pokeSwapFail: "You can't swap to a pokemon that has fainted."
+}
+
