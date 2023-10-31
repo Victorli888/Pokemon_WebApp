cd89080bd1cac9cf1ba4630bd5b506e9c56abcee
victorli <victorli7462@gmail.com>
2023-10-31 11:14:07 -0700

    Hooking in PlayerPokemonTeam from GameData instead of hard code value

--- src/playerPoke.js
+++ src/playerPoke.js
@@ -1,20 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './playerPoke.css';
+import {playerPokemonTeam} from "./gameData";
 
 const PlayerPoke = () => {
-    const [playerPokemon, setPlayerPokemon] = useState([
-        { name: "None1", currentHP: 0, level: 0, spriteUrl: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png", gender: '' },
-        { name: "None2", currentHP: 0, level: 0, spriteUrl: null, gender: '' },
-        { name: "None3", currentHP: 0, level: 0, spriteUrl: null, gender: '' },
-        { name: "None4", currentHP: 0, level: 0, spriteUrl: null, gender: '' },
-        { name: "None5", currentHP: 0, level: 0, spriteUrl: null, gender: '' },
-        { name: "None6", currentHP: 0, level: 0, spriteUrl: null, gender: '' }
-    ]);
+    const [pokemonTeam, setPokemonTeam] = useState(playerPokemonTeam);
 
     // Fetch and set the Pokemon data for each slot
     useEffect(() => {
         const fetchPokemonData = async () => {
-            const promises = playerPokemon.map(async (pokemonSlot, index) => {
+            const promises = pokemonTeam.map(async (pokemonSlot, index) => {
                 const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonSlot.name}`);
                 if (response.ok) {
                     const data = await response.json();
@@ -26,7 +20,7 @@ const PlayerPoke = () => {
             });
 
             const updatedPokemon = await Promise.all(promises);
-            setPlayerPokemon(updatedPokemon);
+            setPokemonTeam(updatedPokemon);
         };
 
         fetchPokemonData();
@@ -34,49 +28,49 @@ const PlayerPoke = () => {
 
     return (
         <div id="player-pokemon">
-            <h1>Player's Pokémon:</h1>
+            <h1>Player's Pokémon Team:</h1>
             <div className="player-pokemon-row">
-                {playerPokemon.slice(0, 2).map((item, index) => (
+                {pokemonTeam.slice(0, 2).map((pokemon, index) => (
                     <div key={index} className="player-pokemon-slot">
                         <div className="image-level-container">
-                            <div id="pokeImg"> <img src={item.spriteUrl} alt={item.name} /></div>
-                            <div id="level"> <p >Level: {item.level}</p> </div>
+                            <div id="pokeImg"> <img src={pokemon.spriteUrl} alt={pokemon.name} /></div>
+                            <div id="level"> <p >Level: {pokemon.level}</p> </div>
                         </div>
                         <div className="name-hp-container">
-                            <h3>{item.name}</h3>
-                            <p>HP: {item.currentHP}</p>
+                            <h3>{pokemon.name}</h3>
+                            <p>HP: {pokemon.hp}</p>
                         </div>
-                        <p id='gender'>Gender: {item.gender}</p>
+                        <p id='gender'>Gender: {pokemon.gender}</p>
                     </div>
                 ))}
             </div>
             <div className="player-pokemon-row">
-                {playerPokemon.slice(2, 4).map((item, index) => (
+                {pokemonTeam.slice(2, 4).map((pokemon, index) => (
                     <div key={index} className="player-pokemon-slot">
                         <div className="image-level-container">
-                            <div id="pokeImg"> <img src={item.spriteUrl} alt={item.name} /></div>
-                            <div id="level"> <p >Level: {item.level}</p> </div>
+                            <div id="pokeImg"> <img src={pokemon.spriteUrl} alt={pokemon.name} /></div>
+                            <div id="level"> <p >Level: {pokemon.level}</p> </div>
                         </div>
                         <div className="name-hp-container">
-                            <h3>{item.name}</h3>
-                            <p>HP: {item.currentHP}</p>
+                            <h3>{pokemon.name}</h3>
+                            <p>HP: {pokemon.hp}</p>
                         </div>
-                        <p id='gender'>Gender: {item.gender}</p>
+                        <p id='gender'>Gender: {pokemon.gender}</p>
                     </div>
                 ))}
             </div>
             <div className="player-pokemon-row">
-                {playerPokemon.slice(4, 6).map((item, index) => (
+                {pokemonTeam.slice(4, 6).map((pokemon, index) => (
                     <div key={index} className="player-pokemon-slot">
                         <div className="image-level-container">
-                            <div id="pokeImg"> <img src={item.spriteUrl} alt={item.name} /></div>
-                            <div id="level"> <p >Level: {item.level}</p> </div>
+                            <div id="pokeImg"> <img src={pokemon.spriteUrl} alt={pokemon.name} /></div>
+                            <div id="level"> <p >Level: {pokemon.level}</p> </div>
                         </div>
                         <div className="name-hp-container">
-                            <h3>{item.name}</h3>
-                            <p>HP: {item.currentHP}</p>
+                            <h3>{pokemon.name}</h3>
+                            <p>HP: {pokemon.hp}</p>
                         </div>
-                        <p id='gender'>Gender: {item.gender}</p>
+                        <p id='gender'>Gender: {pokemon.gender}</p>
                     </div>
                 ))}
             </div>
