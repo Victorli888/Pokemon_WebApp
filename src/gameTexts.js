b1652ed1537544f76cc3ffb7aa31d63420013bcf
victorli <victorli7462@gmail.com>
2024-01-01 19:20:35 -0800

    Remove legacy pokeBattle Code

--- src/gameTexts.js
+++
@@ -1,9 +0,0 @@
-export var  gameTexts = {
-    wildPokemonIntro: "A wild Pokemon has Appeared!",
-    escapeSuccess: "You have successfully run away!",
-    escapeFail: "You are not able to run away from a trainer battle.",
-    pokeSwapFail: "You can't swap to a pokemon that has fainted.",
-    playerDefeated: `Everyone on your team has fainted... \nGame Over, You will need to restart the game`,
-    opponentDefeated: `Your opponent has no other pokemon to fight with... \nYou Win.`
-}
-
